cbdbea22c172cab35d6895e1f3db5b93bdf35c32
I. Jose Kpadeh <ijosekpadeh@gmail.com>
2023-07-21 07:36:17 -0700

    Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -4,11 +4,11 @@ void positive_or_negative(int i)
 {
      if (i > 0)
         
-                printf("%d is positive\n", i);
+                printf("%d is zero\n", i);
         
      else if (i < 0)
         
-                printf("%d is Negative\n", i);
+                printf("%d is zero\n", i);
         
      else
         
